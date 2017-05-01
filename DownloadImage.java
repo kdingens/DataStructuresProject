47a46314d0b325567bd75362c408b0b0db182f90
kdingens <kdingens@nd.edu>
2017-04-30 20:38:03 -0400

    Create DownloadImage.java

---
+++ DownloadImage.java
@@ -0,0 +1,24 @@
+public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImage(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        try {
+            InputStream in = new java.net.URL(urldisplay).openStream();
+            mIcon11 = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
+    }
+}
