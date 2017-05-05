d28ea407017415ed6794333fd529e09a717242cc
kdingens <kdingens@nd.edu>
2017-05-05 02:58:32 -0400

    Create DisplayPopular.java

---
+++ DisplayPopular.java
@@ -0,0 +1,16 @@
+package com.example.kevindingens.firstapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kevindingens on 4/24/17.
+ */
+
+public class DisplayPopular extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.displaypopular);
+    }
+}
